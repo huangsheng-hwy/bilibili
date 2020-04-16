4fe67dfd063afa0c4f3c78f49aba01765243971a
kanmendashu2020 <1957875073@qq.com>
2020-04-17 00:33:38 +0800

    pandas

--- python2excel/createexcel.py
+++ python2excel/createexcel.py
@@ -1,21 +1,29 @@
 import xlwings
 
-excelApp = xlwings.App(False, False)
-excelFile = excelApp.books.add()
+
 
 
 # 创建excel表格
 def createExcel(excel_name):
+    excelApp = xlwings.App(False, False)
+    excelFile = excelApp.books.add()
     excelFile.save(excel_name)
+    excelFile.close()
+    excelApp.quit()
+
 
 
 # 给excel表格增加sheet
 def addSheet(excel_name):
+    excelApp = xlwings.App(False, False)
+    excelFile = excelApp.books.add()
     excelFile.sheets.add("test")
     excelFile.save("1.xlsx")
 
 
 if __name__ == '__main__':
-    excel_name = "1.xlsx"
-    createExcel(excel_name)
-    addSheet(excel_name)
+    for i in range(0,4):
+        excel_name = "d:\\test\\{}.xlsx".format(i)
+        createExcel(excel_name)
+
+    # addSheet(excel_name)
---
+++ python2excel/readexcel.py
@@ -0,0 +1,31 @@
+import os
+
+
+# rootdir = 'd:\\test'
+# list = os.listdir(rootdir) #列出文件夹下所有的目录与文件
+# for i in range(0,len(list)):
+#        path = os.path.join(rootdir,list[i])
+#        print(path)
+       # if os.path.isfile(path):
+       #     print()
+       #     pass
+
+
+# https://www.jianshu.com/p/e21894fc5501
+
+
+import xlwings as xw
+import pandas as pd
+
+file = 'd:\\test\\1.xlsx'
+
+df = pd.DataFrame([[1,2],[3,4]],
+             columns=['c1','c2'],
+             index=['r1','r2'])
+wb = xw.Book()
+sheet = wb.sheets['Sheet1']
+sheet.range('A1').value = df
+# sheet.range('A1').options(pd.DataFrame,expand='table').value
+wb.save(path=file)
+wb.close()
+
