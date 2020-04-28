f0fe94432b240956090573441934b041532dbd1b
kanmendashu2020 <1957875073@qq.com>
2020-04-28 23:56:11 +0800

    Update

--- django_sys/数据可视化_Django初始化世界地图/project/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from . import views
-
-urlpatterns = [
-    url(r'^django_demo', views.django_demo),
-    url(r'^data_demo', views.data_demo),
-]
